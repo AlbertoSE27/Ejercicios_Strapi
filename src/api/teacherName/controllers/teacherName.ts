f0ba25bfe6fe6c894fd69a7dc464692c971acc36
Alberto <albertose_2704@hotmail.com>
2025-03-31 09:18:58 +0200

    "change fixed"

--- src/api/teacherName/controllers/teacherName.ts
+++ src/api/teacherName/controllers/teacherName.ts
@@ -2,18 +2,21 @@ module.exports = {
   async getEventsByTeacher(ctx) {
     try {
       const { teacherName, teacherId } = ctx.request.params;
-      if (!teacherName || teacherId) {
-        return ctx.notFound("Falta el nombre e ID del profesor");
+      if (!teacherName || !teacherId) {
+        return ctx.notFound("Falta el nombre o ID del profesor");
       }
-      const EventsByTeacher = await strapi.documents("api::teacher.teacher").findOne({
-        docuementId: teacherId,
-        populate: eventos: {tittle},
+      const eventsByTeacher = await strapi
+        .documents("api::teacher.teacher")
+        .findOne({
+          documentId: teacherId,
+          populate: { eventos: { fields: ["tittle"] } },
         });
-      if (!EventsByTeacher) {
+      if (!eventsByTeacher) {
         return ctx.notFound("No se encontraron eventos para el profesor");
       }
-      return ctx.send(EventsByTeacher);
-    } catch (error) {
+      return ctx.send(eventsByTeacher);
+    } catch (error: any) {
+      console.error("Error al buscar los eventos:", error);
       return ctx.throw(500, "Error al buscar los eventos");
     }
   },
